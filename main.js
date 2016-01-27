e79057f9911764f76db5a9b4707d22e628a84dbf
Karan Kotwal <kotwalkaran64@gmail.com>
2016-01-27 13:39:18 +0000

    Making sure

--- main.js
+++ main.js
@@ -8,7 +8,19 @@ var zenStyle = [['body', 'navbar-padding-3'], ['body', 'footer-padding-2'], ['se
 ['body','background-image-zen'],['body','background-size-percent-30'],['body', 'background-repeat-no-repeat'],['body', 'background-fixed'],['body','background-position-right-bottom'],['.zen-resources', 'navbarfixed'], ['.resources', 'title'], ['.zen-validate-html', 'buttonLink'], ['.zen-validate-css', 'buttonLink'], ['.zen-license', 'buttonLink'],['.zen-accessibility', 'buttonLink'], ['.zen-github', 'buttonLink'],['.zen-validate-html', 'verticallyCenter'], ['.zen-validate-html', 'linkSpacing'], ['.zen-validate-html', 'complement-text-PaleVioletRed'],
 ['.zen-validate-css', 'verticallyCenter'], ['.zen-validate-css', 'linkSpacing'], ['.zen-validate-css', 'complement-text-PaleVioletRed'], ['.zen-license', 'verticallyCenter'], ['.zen-license', 'linkSpacing'], ['.zen-license', 'complement-text-PaleVioletRed'],
 ['.zen-accessibility', 'verticallyCenter'], ['.zen-accessibility', 'linkSpacing'], ['.zen-accessibility', 'complement-text-PaleVioletRed'], ['.zen-github', 'verticallyCenter'], ['.zen-github', 'linkSpacing'], ['.zen-github', 'complement-text-PaleVioletRed']];
-var styles = [style1, style2, zenStyle];
+
+
+
+var zenStyle2 = [['body', 'navbar-padding-3'], ['body', 'footer-padding-2'], ['section', 'sidebarright-padding-8'],['section', 'center600'], ['section', 'sidebarleft-padding-10'], ['h1', 'set-sans-serif-font-Ubuntu'], ['h2', 'set-sans-serif-font-Ubuntu'],
+['h3', 'set-sans-serif-font-Ubuntu'], ['p', 'complement-serif-font-Ubuntu'], ['.explanation', 'sidebarright-padding-8'], ['.explanation', 'sidebarleft-padding-10'], ['.explanation', 'center600'], ['.participation', 'sidebarright-padding-8'], ['.participation', 'sidebarleft-padding-10'],['.participation', 'center600'], ['.benefits', 'sidebarright-padding-8'], ['.benefits', 'sidebarleft-padding-10'], ['.benefits', 'center600']
+, ['.requirements', 'sidebarright-padding-8'], ['.requirements', 'sidebarleft-padding-10'], ['.requirements', 'center600'], ['.requirements', 'footer-padding-5'], ['footer', 'alignCenter'], ['footer', 'footer'], ['.design-selection', 'navbarrightfixed'],['.design-name', 'font-size-smaller'],['.designer-name', 'font-size-smaller'],['.select', 'invisible'], ['.design-name', 'partA'], ['.designer-name', 'partB'],['.design-archives', 'navbarfixed'], ['archives', 'invisible'], 
+['body','background-image-zen'],['body','background-size-percent-30'],['body', 'background-repeat-no-repeat'],['body', 'background-fixed'],['body','background-position-right-bottom'],['.zen-resources', 'navbarleftfixed'], ['.resources', 'title'], ['.zen-validate-html', 'buttonLink'], ['.zen-validate-css', 'buttonLink'], ['.zen-license', 'buttonLink'],['.zen-accessibility', 'buttonLink'], ['.zen-github', 'buttonLink'],['.zen-validate-html', 'verticallyCenter'], ['.zen-validate-html', 'linkSpacing'], ['.zen-validate-html', 'complement-text-PaleVioletRed'],
+['.zen-validate-css', 'verticallyCenter'], ['.zen-validate-css', 'linkSpacing'], ['.zen-validate-css', 'complement-text-PaleVioletRed'], ['.zen-license', 'verticallyCenter'], ['.zen-license', 'linkSpacing'], ['.zen-license', 'complement-text-PaleVioletRed'],
+['.zen-accessibility', 'verticallyCenter'], ['.zen-accessibility', 'linkSpacing'], ['.zen-accessibility', 'complement-text-PaleVioletRed'], ['.zen-github', 'verticallyCenter'], ['.zen-github', 'linkSpacing'], ['.zen-github', 'complement-text-PaleVioletRed']];
+
+
+
+var styles = [style1, style2, zenStyle, zenStyle2];
 var currentStyleIndex = -1;
 
 var removeStylePair = function(aPair){
